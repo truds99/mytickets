c0e9c3843c429e4218aad70712aab5ff7ab9081b
truds99 <dudscardoso9@gmail.com>
2024-10-26 11:21:04 -0300

    add put and delete event test

--- tests/events.test.ts
+++ tests/events.test.ts
@@ -1,6 +1,8 @@
 import supertest from "supertest";
 import app from '../src/app';
-import { createEventData, createNewEvent } from "./factories/event-factory";
+import { createEventData, createNewEvent, createNewEventName } from "./factories/event-factory";
+import { faker } from "@faker-js/faker/.";
+import prisma from "database";
 
 const api = supertest(app);
 
@@ -8,8 +10,8 @@ describe("GET /events", () => {
 
     it("should return all events", async () => {
 
-        await createNewEvent()
-        await createNewEvent()
+        await createNewEvent();
+        await createNewEvent();
 
         const { body } = await api.get('/events');
 
@@ -30,14 +32,13 @@ describe("GET /events", () => {
 describe("GET /events/:id", () => {
 
     it("should return an specific event", async () => {
-
         const { id } = await createNewEvent();
 
         const { body } = await api.get(`/events/${id}`);
 
         expect(body).toEqual(
             expect.objectContaining({
-                id: expect.any(Number),
+                id,
                 name: expect.any(String),
                 date: expect.any(String),
             })
@@ -65,5 +66,50 @@ describe("POST /events", () => {
 
     })
 })
+
+describe("PUT /events/:id", () => {
+
+    it("should update an event", async () => {
+
+        const { id } = await createNewEvent();
+        const newEvent = await createEventData();
+        
+        const { status, body } = await api.put(`/events/${id}`).send(newEvent);
+
+        expect(status).toBe(200);
+        expect(body).toEqual(
+            expect.objectContaining({
+                id,
+                name: newEvent.name,
+                date: newEvent.date.toISOString()
+            })
+        );
+
+    })
+})
+
+describe("DELETE /events/:id", () => {
+
+    it("should update an event", async () => {
+
+        const { id } = await createNewEvent();
+        
+        const { status } = await api.delete(`/events/${id}`);
+        const eventDeleted = await verifyIfEventWasDeleted(id);
+
+        expect(status).toBe(204);
+        expect(eventDeleted).toBe(true);
+
+    })
+})
+
+async function verifyIfEventWasDeleted(id: number) {
+    const eventDeleted = await prisma.ticket.findUnique({
+        where: {
+            id
+        }
+    });
+    return (!eventDeleted);
+}
 
 
